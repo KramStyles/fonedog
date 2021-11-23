b4c91316898cc838c8a2a33d49513cd8dcc669ff
Michael Jamie <kramstyles@outlook.com>
2021-11-23 16:31:19 +0100

    done with slide design

--- transmission.php
+++ transmission.php
@@ -33,6 +33,7 @@
             border-radius: 25px;
             box-shadow: 6px 6px 10px black;
             margin: 15px 0;
+            position: relative;
         }
         .children img{
             width: 10%;
@@ -47,8 +48,17 @@
             width: 20%;
         }
         .slide-down{
-
+            background-image: linear-gradient(to bottom, #0f3c6e, #0A1F2E 150px);
+            height: 100%;
+            padding: 50px;
+            width: 100%;
+            border-radius: 25px;
+            position: absolute;
+            top: 0;
+            left: 0;
+            /*display: none;*/
         }
+
     </style>
 </head>
 <body>
@@ -58,26 +68,35 @@
             <h1>Phone Transfer</h1>
             <p>Switch files between Android and ios.Easily transfer and backup text Messages,contacts, , videos, music, SMS oncomputer.</p>
             <div class="slide-down"></div>
+            <div class="twins"></div>
         </div>
         <div class="children">
             <img src="images/android-data-recovery.svg" alt="">
             <h1>Android Data Recovery</h1>
             <p>Recover deleted messages, contacts, call logs, photos, videos, whatsapp data from android phone/tablets/SD card.</p>
+            <div class="slide-down"></div>
+            <div class="twins"></div>
         </div>
         <div class="children wid-32">
             <img src="images/ios-data-recovery.svg" alt="">
             <h1>IOs Data Recovery</h1>
             <p>Recover deleted messages, contacts, photos, call history, videos, Whatsapp and much more from iphone.</p>
+            <div class="slide-down"></div>
+            <div class="twins"></div>
         </div>
         <div class="children wid-32">
             <img src="images/ios-system-recovery.svg" alt="">
             <h1>IOS System Recovery</h1>
             <p>Repair your iphones and ipad back to normal status without data loss from various ios System issues.</p>
+            <div class="slide-down"></div>
+            <div class="twins"></div>
         </div>
         <div class="children wid-32">
             <img src="images/broken-android-data-extraction.svg" alt="">
             <h1>Broken Android Data Extraction</h1>
             <p>Restore photos, messages, whatsapp, videos, contacts, call history from broken samsung phone.</p>
+            <div class="slide-down"></div>
+            <div class="twins"></div>
         </div>
     </div>
 </body>
